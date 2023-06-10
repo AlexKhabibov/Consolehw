e099ac8a271218374ca0bbc7f64ce50a63dc57e3
AlexKhabibov <alex.lv.u@gmail.com>
2023-06-10 10:24:29 +0300

    example005 Маша и не Маша

---
+++ example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это ж та самая легендарная Маша!");
+}
+else
+{
+    Console.Write("Ты не Маша, пох на тебя, ");
+    Console.WriteLine(username);
+}
